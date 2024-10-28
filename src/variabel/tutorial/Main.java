3b5175dd6c778839a235e8d8dfe3281e9fd34a8f
arif-rizal1122 <rizallarif32@gmail.com>
2024-10-28 17:05:40 +0800

    Variabel, assignment dan deklarasi

---
+++ src/variabel/tutorial/Main.java
@@ -0,0 +1,15 @@
+package variabel.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 10; // assignment memidahkan  nilai 10 ke y
+        int y = 20;
+        int hasil = x + y;
+        System.out.println("hasil x + y" + hasil); // 30
+
+       // deklarasi menetapkan tipe data
+        int b; // deklarasi
+        b = 20;
+        System.out.println("nilai b = " + b);
+    }
+}
