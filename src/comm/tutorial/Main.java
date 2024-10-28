de5fda3b6433094887e56f2217d3c820374f2079
arif-rizal1122 <rizallarif32@gmail.com>
2024-10-28 16:05:45 +0800

    Pengenalan print dan alur eksekusi

---
+++ src/comm/tutorial/Main.java
@@ -0,0 +1,24 @@
+package comm.tutorial;
+
+import java.util.Arrays;
+
+// ini adalah class
+public class Main {
+        // ini adalah method
+    public static void main(String[] args) {
+        // baris ini adl statment
+        System.out.println("hello why 1 : ");
+        System.out.println("hy juga 2 ");
+
+        System.out.print("baris ke 3 : ");
+        System.out.print("baris keempat 4 ");
+
+        System.out.print("baris 5 : ");
+        System.out.println("baris keenam 6");
+
+        System.out.print("baris ke 7 : ngenter manual \n ");
+        System.out.print("baris ke 8 : ");
+
+        System.out.printf("wiro sableng naga geni %d", 212);
+    }
+}
