be58f69404410e345bbd8f789a7161d40700140b
arif-rizal1122 <rizallarif32@gmail.com>
2024-10-28 16:26:36 +0800

    memahami string[] args

---
+++ src/comm2/tutorial/Main.java
@@ -0,0 +1,10 @@
+package comm2.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // args tipe data nya string
+        System.out.println("halo otong" + args[0]);
+
+    }
+}
