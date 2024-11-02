fa585644e2b1a21259bce6fb49666fe68aaf3363
arif-rizal1122 <rizallarif32@gmail.com>
2024-11-02 22:19:07 +0800

    looping pada array

---
+++ src/loopingPadaArray/latihan/Main.java
@@ -0,0 +1,12 @@
+package loopingPadaArray.latihan;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 5, 4, 7, 6, 8};
+
+        for(int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + ", ");
+        }
+
+    }
+}
